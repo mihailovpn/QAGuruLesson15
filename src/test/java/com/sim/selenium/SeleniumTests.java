bb0c8e3f4460a82a45708af6804f495aedbc0465
Павел Михайлов <pavel.mikhaylov@simbirsoft.com>
2021-11-22 00:37:15 +0300

    added first test

--- src/test/java/com/sim/selenium/SeleniumTests.java
+++ src/test/java/com/sim/selenium/SeleniumTests.java
@@ -1,6 +1,20 @@
 package com.sim.selenium;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SeleniumTests {
     //test
-
+    @Test
+    public void SeleniumTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        assertEquals(
+                "Selenium automates browsers. That's it!",
+                driver.getTitle());
+    }
 }
